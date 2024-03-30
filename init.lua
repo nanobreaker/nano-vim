cc503e5a83c08ebe6be164547aebd2b4267a94b3
nanobreaker <nan0br3aker@gmail.com>
2024-03-30 17:50:19 +0200

    add java support

--- init.lua
+++ init.lua
@@ -1,2 +1,18 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+require("java").setup()
+require("lspconfig").jdtls.setup({
+  settings = {
+    java = {
+      configuration = {
+        runtimes = {
+          {
+            name = "openjdk21",
+            path = "/usr/lib/jvm/java-21-openjdk",
+            default = true,
+          },
+        },
+      },
+    },
+  },
+})
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,6 +4,27 @@
 
 local map = vim.keymap.set
 
+-- Resize window using <ctrl> arrow keys
+map("n", "<A-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+map("n", "<A-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+map("n", "<A-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+map("n", "<A-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+
+-- Move to window using the <ctrl> arrow keys
+map("n", "<C-Left>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+map("n", "<C-Down>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+map("n", "<C-Up>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+map("n", "<C-Right>", "<C-w>l", { desc = "Go to Right Window", remap = true })
+
+-- terminal mapping, <ctrl> arrows keys
+map("t", "<esc><esc>", "<c-\\><c-n>", { desc = "Enter Normal Mode" })
+map("t", "<C-Left>", "<cmd>wincmd h<cr>", { desc = "Go to Left Window" })
+map("t", "<C-Down>", "<cmd>wincmd j<cr>", { desc = "Go to Lower Window" })
+map("t", "<C-Up>", "<cmd>wincmd k<cr>", { desc = "Go to Upper Window" })
+map("t", "<C-Right>", "<cmd>wincmd l<cr>", { desc = "Go to Right Window" })
+map("t", "<C-/>", "<cmd>close<cr>", { desc = "Hide Terminal" })
+map("t", "<c-_>", "<cmd>close<cr>", { desc = "which_key_ignore" })
+
 -- nvimtree
 map("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "Nvimtree Toggle window" })
 map("n", "<leader>e", "<cmd>NvimTreeFocus<CR>", { desc = "Nvimtree Focus window" })
